ef0738cfaee2da5d8f1d55f8c8c703b34261b9eb
Christiebelle <cslester@outlook.com>
2018-03-08 10:48:28 +0000

    Enums created

---
+++ src/main/java/Spells.java
@@ -0,0 +1,21 @@
+public enum Spells {
+    FIREBALL(15, "explodes"),
+    LIGHTNINGSTRIKE(12, "electrifies"),
+    FREEZEBREATH(12, "freezes");
+
+    private int damage;
+    private String attack;
+
+    Spells(int damage, String attack) {
+        this.damage = damage;
+        this.attack = attack;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public String getAttack() {
+        return attack;
+    }
+}
---
+++ src/main/java/Treasure.java
@@ -0,0 +1,12 @@
+public abstract class Treasure {
+
+    private String type;
+
+    public Treasure(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
