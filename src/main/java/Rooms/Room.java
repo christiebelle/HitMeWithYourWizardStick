453edace5fe1dd8566c2e4296200442e008fc473
Christiebelle <cslester@outlook.com>
2018-03-08 11:59:48 +0000

    Test room set up and tested. All tests passing

--- src/main/java/Rooms/Room.java
+++ src/main/java/Rooms/Room.java
@@ -1,12 +1,35 @@
 package Rooms;
 
 import Characters.Enemy;
+import Treasures.Treasure;
 
 public class Room {
 
-    Rooms roomname;
-    Rooms roonnum;
+    String roomname;
+    int roonnum;
     Enemy enemy;
+    Treasure treasure;
 
+    public Room(String roomname, int roonnum, Enemy enemy, Treasure treasure) {
+        this.roomname = roomname;
+        this.roonnum = roonnum;
+        this.enemy = enemy;
+        this.treasure = treasure;
+    }
 
+    public String getRoomname() {
+        return roomname;
+    }
+
+    public int getRoonnum() {
+        return roonnum;
+    }
+
+    public Enemy getEnemy() {
+        return enemy;
+    }
+
+    public Treasure getTreasure() {
+        return treasure;
+    }
 }
